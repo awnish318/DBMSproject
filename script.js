9bf3ef85814dae228ebebf5472f1d97dd0eddfcd
awnish318 <awnishkumar6282@gmail.com>
2022-12-27 18:55:36 +0530

    second commit

--- script.js
+++ script.js
@@ -35,4 +35,19 @@ document.querySelector('#search-icon').onclick = () =>{
   document.querySelector('#close').onclick = () =>{
     document.querySelector('#search-form').classList.remove('active');
   }
+
+  
+  var swiper = new Swiper(".home-slider", {
+    spaceBetween: 110,
+    centeredSlides: true,
+    autoplay: {
+      delay: 1500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  });
   
